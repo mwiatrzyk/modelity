4523b968abc2a969b2b92947494be597b130b6e5
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2024-10-15 21:36:13 +0200

    chore: initial version of CircleCI workflow

--- tasks.py
+++ tasks.py
@@ -14,6 +14,12 @@ def test(ctx: invoke.Context):
 
 
 @invoke.task
+def bump(ctx: invoke.Context):
+    """Bump project version."""
+    ctx.run("bumpify bump")
+
+
+@invoke.task
 def format(ctx: invoke.Context):
     """Run code formatting tools."""
     ctx.run("black --line-length=120 .")
