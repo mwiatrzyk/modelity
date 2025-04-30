891b90f4f2427def8955c4ded396e1f586563e72
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2025-04-30 19:15:35 +0200

    feat: add `modelity.mixins` module providing general purpose mixin classes to reduce boilerplate code

--- modelity/_type_descriptors/ipaddr.py
+++ modelity/_type_descriptors/ipaddr.py
@@ -2,15 +2,15 @@ import ipaddress
 
 from typing import Any
 
-from modelity.interface import IDumpFilter
 from modelity.loc import Loc
 from modelity.unset import Unset
 from modelity.error import Error, ErrorFactory
+from modelity.mixins import StrDumpMixin, NoValidateMixin
 
 
 def make_ipv4_address_type_descriptor():
 
-    class IPv4TypeDescriptor:
+    class IPv4TypeDescriptor(StrDumpMixin, NoValidateMixin):
         def parse(self, errors: list[Error], loc: Loc, value: Any):
             if isinstance(value, ipaddress.IPv4Address):
                 return value
@@ -19,11 +19,5 @@ def make_ipv4_address_type_descriptor():
             except ipaddress.AddressValueError:
                 errors.append(ErrorFactory.parsing_error(loc, value, "not a valid IPv4 address", ipaddress.IPv4Address))
                 return Unset
-
-        def dump(self, loc: Loc, value: Any, filter: IDumpFilter):
-            return str(value)
-
-        def validate(self, root, ctx, errors, loc, value):
-            return
 
     return IPv4TypeDescriptor()
