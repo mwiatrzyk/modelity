dbd69a82f6911c5bc695e0e3fd08521462dd649a
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2026-01-26 18:50:27 +0100

    chore: remove `bumpify` from project deps and install it in CI/CD pipeline directly
    Bumpify depends on Modelity, and using it as a dependency was causing
    dependency resolution problems.

--- tasks.py
+++ tasks.py
@@ -29,12 +29,6 @@ def check(ctx):
 
 
 @invoke.task
-def bump(ctx: invoke.Context):
-    """Bump project version."""
-    ctx.run("bumpify bump")
-
-
-@invoke.task
 def format(ctx: invoke.Context):
     """Run code formatting tools."""
     ctx.run("black --line-length=120 .")
